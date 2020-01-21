d38745c2367529f7e7f4dd0ee6d4ddb363b8d4d8
Winston R. Milling <Winston@Milli.ng>
2020-01-20 23:17:59 -0500

    Import NVM from AUR for Arch

--- zshrc.d/100.setup-nvm.sh
+++ zshrc.d/100.setup-nvm.sh
@@ -3,7 +3,9 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
   [ -s "/usr/local/opt/nvm/etc/bash_completion" ] && . "/usr/local/opt/nvm/etc/bash_completion"
   export PATH="$NVM_DIR/versions/node/v$(<$NVM_DIR/alias/default)/bin:$PATH"
   alias nvm="unalias nvm; [ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"; nvm $@"
-else
+elif [ -f "/usr/share/nvm/init-nvm.sh" ]; then
+  source /usr/share/nvm/init-nvm.sh
+else 
   export NVM_DIR="${XDG_CONFIG_HOME/:-$HOME/.}nvm"
   [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 fi
