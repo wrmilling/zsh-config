4f6f2378e39948a8d54aa6c446b24066f0d96fe6
Winston R. Milling <Winston_R_Milling@homedepot.com>
2020-03-10 11:14:50 -0400

    Wrong alias directive for nvm

--- zshrc.d/100.setup-nvm.sh
+++ zshrc.d/100.setup-nvm.sh
@@ -1,7 +1,7 @@
 if [[ "$OSTYPE" == "darwin"* ]]; then
   export NVM_DIR="$HOME/.nvm"
   [ -s "/usr/local/opt/nvm/etc/bash_completion" ] && . "/usr/local/opt/nvm/etc/bash_completion"
-  export PATH="$NVM_DIR/versions/node/v$(<$NVM_DIR/alias/default)/bin:$PATH"
+  export PATH="$NVM_DIR/versions/node/$(<$NVM_DIR/alias/default)/bin:$PATH"
   alias nvm="unalias nvm; [ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"; nvm $@"
 elif [ -f "/usr/share/nvm/init-nvm.sh" ]; then
   source /usr/share/nvm/init-nvm.sh
