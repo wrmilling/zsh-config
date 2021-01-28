2d85ebe62e17df3497be1d648fb14c51c48d014e
Winston R. Milling <Winston@Milli.ng>
2021-01-27 18:54:25 -0600

    Only call gpg launch if it is not running

--- zshrc.d/101.gpg.sh
+++ zshrc.d/101.gpg.sh
@@ -1,3 +1,8 @@
 export GPG_TTY="$(tty)"
 export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
-gpgconf --launch gpg-agent
+
+if ! pgrep -x "gpg-agent" > /dev/null
+then
+  gpgconf --launch gpg-agent
+fi
+
