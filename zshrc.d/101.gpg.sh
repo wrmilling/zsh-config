8b91ed2d664f8cabc54427df9f7eef1932879b35
Winston R. Milling <Winston@Milli.ng>
2021-02-13 11:28:38 -0600

    Only run gpg setup when config exists

--- zshrc.d/101.gpg.sh
+++ zshrc.d/101.gpg.sh
@@ -1,4 +1,6 @@
-export GPG_TTY="$(tty)"
-export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
-gpgconf --launch gpg-agent
-gpg-connect-agent updatestartuptty /bye > /dev/null
+if [ -f "$HOME/.gnupg/gpg.conf" ]; then
+  export GPG_TTY="$(tty)"
+  export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
+  gpgconf --launch gpg-agent
+  gpg-connect-agent updatestartuptty /bye > /dev/null
+fi
